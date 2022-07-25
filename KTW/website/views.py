a5e2bf071c40e92023314d2ec618ceb1c1d7a737
Code Slayer <lashawnaharris@lewisu.edu>
2022-07-25 18:12:53 -0500

    add templates and views

--- KTW/website/views.py
+++ KTW/website/views.py
@@ -1,8 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 #view for home page
 @views.route('/')
+@views.route('/home')
 def home():
-    return "<h1> Test </h1>"
+    return  render_template('homepage.html')
+
+@views.route('/parents')
+def parents_page():
+    return render_template('parents.html')
+
+@views.route('/sign_up')
+def sign_up():
+    return render_template('signup.html')
+
+@views.route('/about_us')
+def about_us():
+    return render_template('About_us.html')
