7167293ee82645160151cd5b8bf3ec412364d159
Code Slayer <lashawnaharris@lewisu.edu>
2022-07-31 19:40:12 -0500

    update to UI

--- KTW/website/views.py
+++ KTW/website/views.py
@@ -19,3 +19,15 @@ def sign_up():
 @views.route('/about_us')
 def about_us():
     return render_template('About_us.html')
+
+@views.route('/dashboard_parent')
+def dash_parent():
+    return render_template('dashboard_parent.html')
+
+@views.route('/dashboard_student')
+def dash_student():
+    return render_template('dashboard_student.html')
+
+@views.route('/dash_layouts')
+def dash_layouts():
+    return render_template('dashboard_layouts.html')
